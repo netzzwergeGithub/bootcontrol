72948c338ebab48c67d0c385d2f828a2012a8f85
respeaker <respeaker@seeed.cc>
2019-03-27 17:49:09 +0000

    auf verschiedene intents verteilt

--- action-bootcontrol.py
+++ action-bootcontrol.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from debug_lib.help_lib import  print_intentMessage
-from debug_lib.help_lib import  save_intentMessage
 import configparser
 from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
@@ -18,10 +16,24 @@ CONFIG_INI = "config.ini"
 
 BOOTCONTROL_INTENT = "mardeh:SystemCommand"
 CONFIRM_INTENT = "mardeh:confirm"
+REPLAY_INTENT = "mardeh:replay"
+CANCEL_INTENT = "mardeh:cancel"
+follow_up_intents = [CONFIRM_INTENT,REPLAY_INTENT,CANCEL_INTENT]
+# String-Template for command to execute
 SUDO_STRING = "sudo shutdown {} +{}"
 
-command_to_execute = None
+# state variables
+command_to_execute = None # command line to be executed after confirming
+requested_command = None # command extracted fom SystemCommand intent
+requested_time_of_execution = 0 # minutes before eexecution of commend
+last_spoken_text = None   # last text send to user. Used on replay-Intent
 
+
+def  reset_command_state():
+    command_to_execute = None # command line to be executed after confirming
+    requested_command = None # command extracted fom SystemCommand intent
+    requested_time_of_execution = 0 # minutes before eexecution of commend
+    last_spoken_text = None   # last text send to user. Used on replay-Intent
 
 class SnipsConfigParser(configparser.SafeConfigParser):
     def to_dict(self):
@@ -37,14 +49,18 @@ def read_configuration_file(configuration_file):
     except (IOError, configparser.Error) as e:
         return dict()
 
+def subscribe_replay_intent_callback(hermes, intentMessage):
+    hermes.publish_continue_session(intentMessage.session_id, last_spoken_text, follow_up_intents)
+
+def subscribe_cancel_intent_callback(hermes, intentMessage):
+    hermes.publish_end_session(intentMessage.session_id, "{} canceled".format(requested_command ))
+    reset_command_state()
+
 def subscribe_confirm_intent_callback(hermes, intentMessage):
     confirm =  intentMessage.slots.confirm.first().value
-    if confirm == "yes":
-        pass
-    else:
-        pass
 
-    hermes.publish_end_session(intentMessage.session_id, "You said '{}".format(confirm))
+    hermes.publish_end_session(intentMessage.session_id, "System will {} in {} minutes".format(requested_command,requested_time_of_execution))
+    os.system(command_to_execute)
 
 def subscribe_bootcontrol_intent_callback(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
@@ -53,33 +69,29 @@ def subscribe_bootcontrol_intent_callback(hermes, intentMessage):
 
 def action_wrapper(hermes, intentMessage, conf):
     session_id = intentMessage.session_id
-    print("action_wrapper")
-    print(sys.version_info)
-    #print_intentMessage(intentMessage)
-    tempDir = "tmp"
-    if not os.path.isdir(tempDir):
-        os.mkdir(tempDir)
-    save_intentMessage(intentMessage, "{}/{}_messageInfo.txt".format(tempDir,session_id))
-    command =  intentMessage.slots.registeredCommand.first().value
+    requested_command =  intentMessage.slots.registeredCommand.first().value
     if intentMessage.slots.timeToExcecute:
         timeToExecute =intentMessage.slots.timeToExcecute.first().value
-        minutesToGo = getMinutesToShutdown(timeToExecute)
-        print (minutesToGo)
+        requested_time_of_execution = getMinutesToShutdown(timeToExecute)
+        print (requested_time_of_execution)
     else:
-        minutesToGo = 0
+        requested_time_of_execution = 0
 
-    if command == "reboot":
+    if requested_command == "reboot":
         shortCommand = "-r"
-    if command == "halt":
+    elif requested_command == "halt":
         shortCommand = "-H"
-    if command == "shutdown":
+    elif requested_command == "shutdown":
         shortCommand = "-P"
+    else:
+        hermes.publish_end_session(intentMessage.session_id, "Wrong command {}".format(requested_command ))
 
-    sentence =  "The system will {} in {} minutes. Confirm with 'yes', abort with 'no'? ".format(command, minutesToGo)
-    hermes.publish_continue_session(intentMessage.session_id, sentence, [CONFIRM_INTENT])
 
-    command_to_execute = SUDO_STRING.format(shortCommand, minutesToGo)
-    #os.system(SUDO_STRING.format(shortCommand, timeToExecute))
+    sentence =  "The system will {} in {} minutes. Confirm with 'yes', abort with 'no'? ".format(requested_command, requested_time_of_execution)
+    hermes.publish_continue_session(intentMessage.session_id, sentence, follow_up_intents)
+
+    command_to_execute = SUDO_STRING.format(shortCommand, requested_time_of_execution)
+    last_spoken_text = sentence 
 
 
 if __name__ == "__main__":
@@ -88,4 +100,6 @@ if __name__ == "__main__":
     with Hermes(mqtt_options=mqtt_opts) as h:
         h.subscribe_intent(BOOTCONTROL_INTENT, subscribe_bootcontrol_intent_callback) \
          .subscribe_intent(CONFIRM_INTENT, subscribe_confirm_intent_callback) \
+         .subscribe_intent(REPLAY_INTENT, subscribe_replay_intent_callback) \
+         .subscribe_intent(CANCEL_INTENT, subscribe_cancel_intent_callback) \
          .start()
