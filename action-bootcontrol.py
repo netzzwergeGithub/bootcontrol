ac5ced634c84c8e4b2926d94b13a69e399ac1878
Maslek <github@netzzwerge.de>
2019-04-03 13:11:51 +0200

    Application-State in Klasse verschoben

--- action-bootcontrol.py
+++ action-bootcontrol.py
@@ -11,6 +11,8 @@ import sys
 import datetime
 import time
 
+from code.ApplicationState import ApplicationState
+
 from  timeconvert import  getMinutesToShutdown
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
@@ -23,19 +25,6 @@ CANCEL_INTENT = "mardeh:cancel"
 follow_up_intents = [CONFIRM_INTENT,REPLAY_INTENT,CANCEL_INTENT]
 # String-Template for command to execute
 SUDO_STRING = "sudo shutdown {} +{}"
-class ApplicationState:
-    # state variables
-    command_to_execute = None # command line to be executed after confirming
-    requested_command = None # command extracted fom SystemCommand intent
-    requested_time_of_execution = 0 # minutes before eexecution of commend
-    last_spoken_text = None   # last text send to user. Used on replay-Intent
-
-
-def  reset_command_state():
-    ApplicationState.command_to_execute = None # command line to be executed after confirming
-    ApplicationState.requested_command = None # command extracted fom SystemCommand intent
-    ApplicationState.requested_time_of_execution = 0 # minutes before eexecution of commend
-    ApplicationState.last_spoken_text = None   # last text send to user. Used on replay-Intent
 
 class SnipsConfigParser(configparser.SafeConfigParser):
     def to_dict(self):
@@ -53,29 +42,27 @@ def read_configuration_file(configuration_file):
 
 def subscribe_session_started(hermes, intentMessage):
     print("Session started\n")
-    reset_command_state()
+    ApplicationState.resetCommandState()
 
 def subscribe_session_ended(hermes, intentMessage):
     print("Session Ended\n")
-    if ApplicationState.command_to_execute != None:
-        print(ApplicationState.command_to_execute)
-        os.system(ApplicationState.command_to_execute)
-    reset_command_state()
+    command = ApplicationState.getCommandToExecute()
+    if command != None:
+        print(command)
+        os.system(command)
+    ApplicationState.resetCommandState()
 
 def subscribe_replay_intent_callback(hermes, intentMessage):
-    hermes.publish_continue_session(intentMessage.session_id, ApplicationState.last_spoken_text, follow_up_intents)
+    hermes.publish_continue_session(intentMessage.session_id, ApplicationState.getlastSpokenText(), follow_up_intents)
 
 def subscribe_cancel_intent_callback(hermes, intentMessage):
-    #print(ApplicationState.requested_command)
-    hermes.publish_end_session(intentMessage.session_id, "{} canceld".format(ApplicationState.requested_command ))
-    reset_command_state()
+    hermes.publish_end_session(intentMessage.session_id, "{} canceld".format(ApplicationState.getRequestedCommand() ))
+    ApplicationState.resetCommandState()
 
 def subscribe_confirm_intent_callback(hermes, intentMessage):
-    #print(ApplicationState.requested_command)
     confirm =  intentMessage.slots.confirm.first().value
 
-    hermes.publish_end_session(intentMessage.session_id, "System will {} in {} minutes".format(ApplicationState.requested_command,ApplicationState.requested_time_of_execution))
-    # os.system(ApplicationState.command_to_execute)
+    hermes.publish_end_session(intentMessage.session_id, "System will {} in {} minutes".format(ApplicationState.getRequestedCommand(),ApplicationState.getRequestedTimeOfExecution()))
 
 def subscribe_bootcontrol_intent_callback(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
@@ -84,31 +71,30 @@ def subscribe_bootcontrol_intent_callback(hermes, intentMessage):
 
 def action_wrapper(hermes, intentMessage, conf):
     session_id = intentMessage.session_id
-    ApplicationState.requested_command =  intentMessage.slots.registeredCommand.first().value
+    ApplicationState.setRequestedCommand(intentMessage.slots.registeredCommand.first().value)
     if intentMessage.slots.timeToExcecute:
         timeToExecute =intentMessage.slots.timeToExcecute.first().value
-        ApplicationState.requested_time_of_execution = getMinutesToShutdown(timeToExecute)
-        #print (ApplicationState.requested_time_of_execution)
+        ApplicationState.setRequestedTimeOfExecution(getMinutesToShutdown(timeToExecute))
+        #print (ApplicationState.getRequestedTimeOfExecution())
     else:
-        ApplicationState.requested_time_of_execution = 0
+        ApplicationState.setRequestedTimeOfExecution(0)
 
-    if ApplicationState.requested_command == "reboot":
+    if ApplicationState.getRequestedCommand() == "reboot":
         shortCommand = "-r"
-    elif ApplicationState.requested_command == "halt":
+    elif ApplicationState.getRequestedCommand() == "halt":
         shortCommand = "-H"
-    elif ApplicationState.requested_command == "shutdown":
+    elif ApplicationState.getRequestedCommand() == "shutdown":
         shortCommand = "-P"
     else:
-        hermes.publish_end_session(intentMessage.session_id, "Wrong command {}".format(ApplicationState.requested_command ))
+        hermes.publish_end_session(intentMessage.session_id, "Wrong command {}".format(ApplicationState.getRequestedCommand() ))
 
 
-    sentence =  "The system will {} in {} minutes. Confirm with 'yes', abort with 'no', replay with 'repeat' ".format(ApplicationState.requested_command, ApplicationState.requested_time_of_execution)
+    sentence =  "The system will {} in {} minutes. Confirm with 'yes', abort with 'no', replay with 'repeat' ".format(ApplicationState.getRequestedCommand(), ApplicationState.getRequestedTimeOfExecution())
     hermes.publish_continue_session(intentMessage.session_id, sentence, follow_up_intents)
 
-    ApplicationState.command_to_execute = SUDO_STRING.format(shortCommand, ApplicationState.requested_time_of_execution)
-    #print(ApplicationState.command_to_execute )
+    ApplicationState.setCommandToExecute(SUDO_STRING.format(shortCommand, ApplicationState.getRequestedTimeOfExecution()))
 
-    ApplicationState.last_spoken_text = sentence 
+    ApplicationState.setlastSpokenText(sentence) 
 
 
 if __name__ == "__main__":
---
+++ code/ApplicationState.py
@@ -0,0 +1,45 @@
+class ApplicationState:
+    # state variables
+    command_to_execute = None # command line to be executed after confirming
+    requested_command = None # command extracted fom SystemCommand intent
+    requested_time_of_execution = 0 # minutes before eexecution of commend
+    last_spoken_text = None   # last text send to user. Used on replay-Intent
+
+
+    def  resetCommandState(cls):
+        cls.command_to_execute = None # command line to be executed after confirming
+        cls.requested_command = None # command extracted fom SystemCommand intent
+        cls.requested_time_of_execution = 0 # minutes before eexecution of commend
+        cls.last_spoken_text = None   # last text send to user. Used on replay-Intent
+
+    @classmethod
+    def setlastSpokenText(cls,text):
+        cls.last_spoken_text = text
+
+    @classmethod
+    def setCommandToExecute(cls,command):
+        cls.command_to_execute = command
+
+    @classmethod
+    def setRequestedCommand(cls,command):
+        cls.requested_command = command
+
+    @classmethod
+    def setRequestedTimeOfExecution(cls, timeToExcecute):
+        cls.requested_time_of_execution = timeToExecute
+
+    @classmethod
+    def getlastSpokenText(cls):
+        return cls.last_spoken_text
+
+    @classmethod
+    def getCommandToExecute(cls):
+        return cls.command_to_execute
+
+    @classmethod
+    def getRequestedCommand(cls):
+        return cls.requested_command
+
+    @classmethod
+    def getRequestedTimeOfExecution(cls):
+        return cls.requested_time_of_execution
