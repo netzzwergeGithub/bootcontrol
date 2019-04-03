ab9df4a18dbf64fca68ce09b56fdd6b787f582d6
respeaker <respeaker@seeed.cc>
2019-04-03 11:27:04 +0100

    system command erst nach session ende. Status jetzt in Klasse.

--- action-bootcontrol.py
+++ action-bootcontrol.py
@@ -9,6 +9,8 @@ import io
 import os
 import sys
 import datetime
+import time
+
 from  timeconvert import  getMinutesToShutdown
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
@@ -21,19 +23,19 @@ CANCEL_INTENT = "mardeh:cancel"
 follow_up_intents = [CONFIRM_INTENT,REPLAY_INTENT,CANCEL_INTENT]
 # String-Template for command to execute
 SUDO_STRING = "sudo shutdown {} +{}"
-
-# state variables
-command_to_execute = None # command line to be executed after confirming
-requested_command = None # command extracted fom SystemCommand intent
-requested_time_of_execution = 0 # minutes before eexecution of commend
-last_spoken_text = None   # last text send to user. Used on replay-Intent
-
-
-def  reset_command_state():
+class ApplicationState:
+    # state variables
     command_to_execute = None # command line to be executed after confirming
     requested_command = None # command extracted fom SystemCommand intent
     requested_time_of_execution = 0 # minutes before eexecution of commend
     last_spoken_text = None   # last text send to user. Used on replay-Intent
+
+
+def  reset_command_state():
+    ApplicationState.command_to_execute = None # command line to be executed after confirming
+    ApplicationState.requested_command = None # command extracted fom SystemCommand intent
+    ApplicationState.requested_time_of_execution = 0 # minutes before eexecution of commend
+    ApplicationState.last_spoken_text = None   # last text send to user. Used on replay-Intent
 
 class SnipsConfigParser(configparser.SafeConfigParser):
     def to_dict(self):
@@ -49,18 +51,31 @@ def read_configuration_file(configuration_file):
     except (IOError, configparser.Error) as e:
         return dict()
 
+def subscribe_session_started(hermes, intentMessage):
+    print("Session started\n")
+    reset_command_state()
+
+def subscribe_session_ended(hermes, intentMessage):
+    print("Session Ended\n")
+    if ApplicationState.command_to_execute != None:
+        print(ApplicationState.command_to_execute)
+        os.system(ApplicationState.command_to_execute)
+    reset_command_state()
+
 def subscribe_replay_intent_callback(hermes, intentMessage):
-    hermes.publish_continue_session(intentMessage.session_id, last_spoken_text, follow_up_intents)
+    hermes.publish_continue_session(intentMessage.session_id, ApplicationState.last_spoken_text, follow_up_intents)
 
 def subscribe_cancel_intent_callback(hermes, intentMessage):
-    hermes.publish_end_session(intentMessage.session_id, "{} canceled".format(requested_command ))
+    #print(ApplicationState.requested_command)
+    hermes.publish_end_session(intentMessage.session_id, "{} canceld".format(ApplicationState.requested_command ))
     reset_command_state()
 
 def subscribe_confirm_intent_callback(hermes, intentMessage):
+    #print(ApplicationState.requested_command)
     confirm =  intentMessage.slots.confirm.first().value
 
-    hermes.publish_end_session(intentMessage.session_id, "System will {} in {} minutes".format(requested_command,requested_time_of_execution))
-    os.system(command_to_execute)
+    hermes.publish_end_session(intentMessage.session_id, "System will {} in {} minutes".format(ApplicationState.requested_command,ApplicationState.requested_time_of_execution))
+    # os.system(ApplicationState.command_to_execute)
 
 def subscribe_bootcontrol_intent_callback(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
@@ -69,37 +84,40 @@ def subscribe_bootcontrol_intent_callback(hermes, intentMessage):
 
 def action_wrapper(hermes, intentMessage, conf):
     session_id = intentMessage.session_id
-    requested_command =  intentMessage.slots.registeredCommand.first().value
+    ApplicationState.requested_command =  intentMessage.slots.registeredCommand.first().value
     if intentMessage.slots.timeToExcecute:
         timeToExecute =intentMessage.slots.timeToExcecute.first().value
-        requested_time_of_execution = getMinutesToShutdown(timeToExecute)
-        print (requested_time_of_execution)
+        ApplicationState.requested_time_of_execution = getMinutesToShutdown(timeToExecute)
+        #print (ApplicationState.requested_time_of_execution)
     else:
-        requested_time_of_execution = 0
+        ApplicationState.requested_time_of_execution = 0
 
-    if requested_command == "reboot":
+    if ApplicationState.requested_command == "reboot":
         shortCommand = "-r"
-    elif requested_command == "halt":
+    elif ApplicationState.requested_command == "halt":
         shortCommand = "-H"
-    elif requested_command == "shutdown":
+    elif ApplicationState.requested_command == "shutdown":
         shortCommand = "-P"
     else:
-        hermes.publish_end_session(intentMessage.session_id, "Wrong command {}".format(requested_command ))
+        hermes.publish_end_session(intentMessage.session_id, "Wrong command {}".format(ApplicationState.requested_command ))
 
 
-    sentence =  "The system will {} in {} minutes. Confirm with 'yes', abort with 'no'? ".format(requested_command, requested_time_of_execution)
+    sentence =  "The system will {} in {} minutes. Confirm with 'yes', abort with 'no', replay with 'repeat' ".format(ApplicationState.requested_command, ApplicationState.requested_time_of_execution)
     hermes.publish_continue_session(intentMessage.session_id, sentence, follow_up_intents)
 
-    command_to_execute = SUDO_STRING.format(shortCommand, requested_time_of_execution)
-    last_spoken_text = sentence 
+    ApplicationState.command_to_execute = SUDO_STRING.format(shortCommand, ApplicationState.requested_time_of_execution)
+    #print(ApplicationState.command_to_execute )
+
+    ApplicationState.last_spoken_text = sentence 
 
 
 if __name__ == "__main__":
-    print(sys.version_info)
     mqtt_opts = MqttOptions()
     with Hermes(mqtt_options=mqtt_opts) as h:
         h.subscribe_intent(BOOTCONTROL_INTENT, subscribe_bootcontrol_intent_callback) \
          .subscribe_intent(CONFIRM_INTENT, subscribe_confirm_intent_callback) \
          .subscribe_intent(REPLAY_INTENT, subscribe_replay_intent_callback) \
          .subscribe_intent(CANCEL_INTENT, subscribe_cancel_intent_callback) \
+         .subscribe_session_started( subscribe_session_started) \
+         .subscribe_session_ended( subscribe_session_ended) \
          .start()
